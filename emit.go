57efecba6103e2f87e2e521001e251e42ff2b752
xuender <xuender@gmail.com>
2024-09-12 20:10:24 +0800

    feat: emit remove steps

--- emit.go
+++ emit.go
@@ -2,11 +2,7 @@ package flow
 
 import "iter"
 
-func Emit[E any](seq iter.Seq[E], steps ...Step[E, E]) {
-	for _, step := range steps {
-		seq = step(seq)
-	}
-
+func Emit[E any](seq iter.Seq[E]) {
 	call := func(E) bool {
 		return true
 	}
