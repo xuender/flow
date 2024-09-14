1cef250e55f86d22b895173d3dcb444e153c79aa
xuender <xuender@gmail.com>
2024-09-14 13:18:46 +0800

    test: cover

--- seq/first_example_test.go
+++ seq/first_example_test.go
@@ -8,7 +8,9 @@ import (
 
 func ExampleFirst() {
 	fmt.Println(seq.First(seq.Range(100)))
+	fmt.Println(seq.First(seq.Range(0)))
 
 	// Output:
 	// 0 true
+	// 0 false
 }
--- seq/limit_example_test.go
+++ seq/limit_example_test.go
@@ -7,8 +7,22 @@ import (
 )
 
 func ExampleLimit() {
-	fmt.Println(seq.Join(seq.Limit(seq.Range(10), 3), ","))
+	for num := range seq.Limit(seq.Range(10), 3) {
+		if num > 1 {
+			break
+		}
+
+		fmt.Println(num)
+	}
+
+	for num := range seq.Limit(seq.Range(10), 3) {
+		fmt.Println(num)
+	}
 
 	// Output:
-	// 0,1,2
+	// 0
+	// 1
+	// 0
+	// 1
+	// 2
 }
