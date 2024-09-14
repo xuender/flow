f74aa3795f70717c2b439d59bf1461313787cc2d
xuender <xuender@gmail.com>
2024-09-15 07:56:57 +0800

    test: cover

--- parallel_example_test.go
+++ parallel_example_test.go
@@ -21,3 +21,18 @@ func ExampleParallel() {
 	// 0
 	// 3
 }
+
+func ExampleParallel2() {
+	for key, val := range flow.Parallel2(
+		2,
+		seq.Range2(100),
+		flow.Limit2[int, int](3),
+		flow.Filter2(func(key, _ int) bool { return key%3 == 0 }),
+	) {
+		fmt.Println(key, val)
+	}
+
+	// Output:
+	// 0 0
+	// 3 3
+}
--- seq/chan_example_test.go
+++ seq/chan_example_test.go
@@ -26,6 +26,26 @@ func ExampleChan() {
 	// 1
 }
 
+func ExampleChan2() {
+	cha := make(chan seq.Tuple[int, int], 3)
+	cha <- seq.T(1, 1)
+	cha <- seq.T(2, 2)
+	cha <- seq.T(3, 3)
+
+	close(cha)
+
+	for key, val := range seq.Chan2(cha) {
+		if key > 1 {
+			break
+		}
+
+		fmt.Println(key, val)
+	}
+
+	// Output:
+	// 1 1
+}
+
 func ExampleToChans() {
 	chans := seq.ToChans(seq.Range(10), 3)
 
@@ -43,4 +63,23 @@ func ExampleToChans() {
 	// 0
 	// 1
 	// 2
+}
+
+func ExampleToChans2() {
+	chans := seq.ToChans2(seq.Range2(10), 3)
+
+	for _, cha := range chans {
+		for item := range cha {
+			if item.A > 2 {
+				break
+			}
+
+			fmt.Println(item.A, item.B)
+		}
+	}
+
+	// Output:
+	// 0 0
+	// 1 1
+	// 2 2
 }
