90ccd6871d9f845657c0994a4f193ad7fdbb305d
xuender <xuender@gmail.com>
2024-09-14 16:08:31 +0800

    doc: package

---
+++ flow.go
@@ -0,0 +1,4 @@
+// Package flow implements functionality similar to Java 8 Streams.
+//
+// It utilizes Go 1.23's iterator and generic type support.
+package flow
---
+++ seq/seq.go
@@ -0,0 +1,4 @@
+// Package seq provides functions for handling the iter.Seq interface in Go 1.23.
+//
+// This package aims to simplify iter.Seq operations, such as range, sort, filter, and more.
+package seq
