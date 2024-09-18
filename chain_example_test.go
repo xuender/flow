417a3fb1d79c98818e886d27b56289d753f2c134
xuender <xuender@gmail.com>
2024-09-18 09:45:47 +0800

    test: add Chain2 example

--- chain_example_test.go
+++ chain_example_test.go
@@ -39,3 +39,17 @@ func ExampleChain_map() {
 	// num:0
 	// num:2
 }
+
+func ExampleChain2() {
+	for key, val := range flow.Chain2(
+		seq.Range2(100),
+		flow.Limit2[int, int](3),
+		flow.Filter2(func(key, _ int) bool { return key%2 == 0 }),
+	) {
+		fmt.Println(key, val)
+	}
+
+	// Output:
+	// 0 0
+	// 2 2
+}
