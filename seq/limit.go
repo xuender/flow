adfa1e11196dcd6373159586c2459c8c7035348e
xuender <xuender@gmail.com>
2024-09-27 13:19:39 +0800

    refactor: seq.Limit

--- seq/limit.go
+++ seq/limit.go
@@ -7,18 +7,17 @@ import "iter"
 // Play: https://go.dev/play/p/ZEzCPmRXHrs
 func Limit[V any](input iter.Seq[V], limit int) iter.Seq[V] {
 	return func(yield func(V) bool) {
+		if limit <= 0 {
+			return
+		}
+
 		idx := 0
-
 		for item := range input {
-			if idx >= limit {
-				return
-			}
-
-			if !yield(item) {
-				return
-			}
-
 			idx++
+
+			if !yield(item) || idx >= limit {
+				return
+			}
 		}
 	}
 }
@@ -28,18 +27,17 @@ func Limit[V any](input iter.Seq[V], limit int) iter.Seq[V] {
 // Play: https://go.dev/play/p/Cgz_vpTJfQv
 func Limit2[K, V any](input iter.Seq2[K, V], limit int) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
+		if limit <= 0 {
+			return
+		}
+
 		idx := 0
-
 		for key, val := range input {
-			if idx >= limit {
-				return
-			}
-
-			if !yield(key, val) {
-				return
-			}
-
 			idx++
+
+			if !yield(key, val) || idx >= limit {
+				return
+			}
 		}
 	}
 }
